d626c286e4b853175865467acf8df54faf49e5fc
Kanasi <Kanstantsin.Asinouski@gmail.com>
2024-09-03 15:14:53 +0200

    Added styles and changed code base

--- Frontend/adminpanel/src/components/UI/openImgDialog/OpenImgDialog.jsx
+++ Frontend/adminpanel/src/components/UI/openImgDialog/OpenImgDialog.jsx
@@ -1,26 +1,51 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDropzone } from 'react-dropzone';
 import cl from './OpenImgDialog.module.css'
 
 export default function OpenImgDialog() {
 
-    const { acceptedFiles, getRootProps, getInputProps } = useDropzone();
+    const [files, setFiles] = useState([]);
 
-    const files = acceptedFiles.map(file => (
-        <li key={file.path}>
-            {file.path} - {file.size} bytes
-        </li>
+    const { getRootProps, getInputProps } = useDropzone({
+        accept: {
+            'image/*': []
+        },
+        maxFiles: 1,
+        onDrop: acceptedFiles => {
+            setFiles(acceptedFiles.map(file => Object.assign(file, {
+                preview: URL.createObjectURL(file)
+            })));
+        }
+    });
+
+    const thumbs = files.map(file => (
+        <div key={file.name}>
+            <div className={cl.thumbInner}>
+                <img
+                    alt='Preview'
+                    src={file.preview}
+                    className={cl.previewImg}
+                    // Revoke data uri after image is loaded
+                    onLoad={() => { URL.revokeObjectURL(file.preview) }}
+                />
+            </div>
+        </div>
     ));
 
+    useEffect(() => {
+        // Make sure to revoke the data uris to avoid memory leaks, will run on unmount
+        return () => files.forEach(file => URL.revokeObjectURL(file.preview));
+        // eslint-disable-next-line
+    }, []);
+
     return (
-        <section className="container">
-            <div {...getRootProps({ className: 'dropzone' })}>
+        <section>
+            <div className={cl.dropzone} {...getRootProps()}>
                 <input {...getInputProps()} />
-                <p>Drag 'n' drop some files here, or click to select files</p>
+                <p>Drag 'n' drop file here, or click to select file</p>
             </div>
-            <aside style={cl.thumbsContainer}>
-                <h4>Files</h4>
-                <ul>{files}</ul>
+            <aside>
+                {thumbs}
             </aside>
         </section>
     )
