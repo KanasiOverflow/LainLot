8a59f99fc27dc4cdfe19a4ebb1e0d91229e9db99
Kanasi <Kanstantsin.Asinouski@gmail.com>
2024-10-21 14:13:49 +0200

    CORS fixing

--- Backend/RestAPI/Program.cs
+++ Backend/RestAPI/Program.cs
@@ -13,13 +13,23 @@ builder.Services.AddControllers().AddNewtonsoftJson(options =>
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+var LocalHostOrigins = "LocalHostOrigins";
+var corsAdresses = new string[]
+{
+    "http://localhost:3000/",
+    "http://localhost:8040/",
+    "https://lainlot.com"
+};
+
 builder.Services.AddCors(options =>
 {
-    options.AddDefaultPolicy(builder =>
-    {
-        builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
-    });
+    options.AddPolicy(name: LocalHostOrigins,
+        policy =>
+        {
+            policy.WithOrigins(corsAdresses).AllowAnyHeader().AllowAnyMethod();
+        });
 });
+
 
 var context = new LainLotContext();
 
@@ -32,21 +42,21 @@ builder.Services.AddAuthorization();
 
 builder.Services.AddSingleton<IRepository<About>>(new Repository<About>(context));
 builder.Services.AddSingleton<IRepository<AccessLevel>>(new Repository<AccessLevel>(context));
-builder.Services.AddSingleton<IRepository<Cart>>(new Repository<Cart>(context)); 
-builder.Services.AddSingleton<IRepository<Category>>(new Repository<Category>(context)); 
-builder.Services.AddSingleton<IRepository<CategoryHierarchy>>(new Repository<CategoryHierarchy>(context)); 
-builder.Services.AddSingleton<IRepository<Color>>(new Repository<Color>(context)); 
+builder.Services.AddSingleton<IRepository<Cart>>(new Repository<Cart>(context));
+builder.Services.AddSingleton<IRepository<Category>>(new Repository<Category>(context));
+builder.Services.AddSingleton<IRepository<CategoryHierarchy>>(new Repository<CategoryHierarchy>(context));
+builder.Services.AddSingleton<IRepository<Color>>(new Repository<Color>(context));
 builder.Services.AddSingleton<IRepository<Contact>>(new Repository<Contact>(context));
-builder.Services.AddSingleton<IRepository<CustomizableProduct>>(new Repository<CustomizableProduct>(context)); 
-builder.Services.AddSingleton<IRepository<CustomizationOrder>>(new Repository<CustomizationOrder>(context)); 
-builder.Services.AddSingleton<IRepository<FabricType>>(new Repository<FabricType>(context)); 
+builder.Services.AddSingleton<IRepository<CustomizableProduct>>(new Repository<CustomizableProduct>(context));
+builder.Services.AddSingleton<IRepository<CustomizationOrder>>(new Repository<CustomizationOrder>(context));
+builder.Services.AddSingleton<IRepository<FabricType>>(new Repository<FabricType>(context));
 builder.Services.AddSingleton<IRepository<Language>>(new Repository<Language>(context));
-builder.Services.AddSingleton<IRepository<Order>>(new Repository<Order>(context)); 
-builder.Services.AddSingleton<IRepository<OrderHistory>>(new Repository<OrderHistory>(context)); 
-builder.Services.AddSingleton<IRepository<OrderStatus>>(new Repository<OrderStatus>(context)); 
-builder.Services.AddSingleton<IRepository<Payment>>(new Repository<Payment>(context)); 
+builder.Services.AddSingleton<IRepository<Order>>(new Repository<Order>(context));
+builder.Services.AddSingleton<IRepository<OrderHistory>>(new Repository<OrderHistory>(context));
+builder.Services.AddSingleton<IRepository<OrderStatus>>(new Repository<OrderStatus>(context));
+builder.Services.AddSingleton<IRepository<Payment>>(new Repository<Payment>(context));
 builder.Services.AddSingleton<IRepository<Product>>(new Repository<Product>(context));
-builder.Services.AddSingleton<IRepository<ProductImage>>(new Repository<ProductImage>(context)); 
+builder.Services.AddSingleton<IRepository<ProductImage>>(new Repository<ProductImage>(context));
 builder.Services.AddSingleton<IRepository<ProductTranslation>>(new Repository<ProductTranslation>(context));
 builder.Services.AddSingleton<IRepository<Review>>(new Repository<Review>(context));
 builder.Services.AddSingleton<IRepository<User>>(new Repository<User>(context));
@@ -65,7 +75,9 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseCors();
+app.UseRouting();
+
+app.UseCors(LocalHostOrigins);
 
 app.UseAuthentication();
 
