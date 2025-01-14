a730766673172097212da30d9e58ecb329950df6
Kanasi <Kanstantsin.Asinouski@gmail.com>
2025-01-14 13:43:29 +0100

    Add new models based on database tables list

---
+++ Backend/RestAPI/Models/CustomSleefe.cs
@@ -0,0 +1,10 @@
+﻿namespace RestAPI.Models;
+
+public partial class CustomSleefe
+{
+    public int Id { get; set; }
+
+    public int FkBaseSleeves { get; set; }
+
+    public string? CustomSettings { get; set; }
+}
