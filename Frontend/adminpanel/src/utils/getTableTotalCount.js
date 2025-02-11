d6929df3f0fe5ddae11890e7d63b399a9c08f77d
Kanasi <Kanstantsin.Asinouski@gmail.com>
2025-02-11 11:59:45 +0100

    Modify imports

--- Frontend/adminpanel/src/utils/getTableTotalCount.js
+++ Frontend/adminpanel/src/utils/getTableTotalCount.js
@@ -1,25 +1,48 @@
 import AboutService from 'api/CRUD/AboutService';
 import AccessLevelsService from 'api/CRUD/AccessLevelsService';
+import BaseBeltsService from 'api/CRUD/BaseBeltsService';
+import BaseNecklinesService from 'api/CRUD/BaseNecklinesService';
+import BasePantsCuffsService from 'api/CRUD/BasePantsCuffsService';
+import BasePantsService from 'api/CRUD/BasePantsService';
+import BaseSleeveCuffsService from 'api/CRUD/BaseSleeveCuffsService';
+import BaseSleevesService from 'api/CRUD/BaseSleevesService';
+import BaseSportSuitService from 'api/CRUD/BaseSportSuitService';
+import BaseSweatersService from 'api/CRUD/BaseSweatersService';
 import CartService from 'api/CRUD/CartService';
 import CategoriesService from 'api/CRUD/CategoriesService';
 import CategoryHierarchyService from 'api/CRUD/CategoryHierarchyService';
 import ColorsService from 'api/CRUD/ColorsService';
 import ContactsService from 'api/CRUD/ContactsService';
+import CountriesService from 'api/CRUD/CountriesService';
+import CurrenciesService from 'api/CRUD/CurrenciesService';
+import CustomBeltsService from 'api/CRUD/CustomBeltsService';
 import CustomizableProductsService from 'api/CRUD/CustomizableProductsService';
-import CustomizationOrdersService from 'api/CRUD/CustomizationOrdersService';
+import CustomNecklinesService from 'api/CRUD/CustomNecklinesService';
+import CustomPantsCuffsService from 'api/CRUD/CustomPantsCuffsService';
+import CustomPantsService from 'api/CRUD/CustomPantsService';
+import CustomSleeveCuffsService from 'api/CRUD/CustomSleeveCuffsService';
+import CustomSleevesService from 'api/CRUD/CustomSleevesService';
+import CustomSportSuitService from 'api/CRUD/CustomSportSuitService';
+import CustomSweatersService from 'api/CRUD/CustomSweatersService';
 import FabricTypesService from 'api/CRUD/FabricTypesService';
 import LanguagesService from 'api/CRUD/LanguagesService';
 import OrdersService from 'api/CRUD/OrdersService';
 import OrderHistoryService from 'api/CRUD/OrderHistoryService';
 import OrderStatusesService from 'api/CRUD/OrderStatusesService';
+import PaymentMethodsService from 'api/CRUD/PaymentMethodsService';
 import PaymentsService from 'api/CRUD/PaymentsService';
-import ProductsService from 'api/CRUD/ProductsService';
+import PaymentStatusesService from 'api/CRUD/PaymentStatusesService';
 import ProductImagesService from 'api/CRUD/ProductImagesService';
+import ProductrdersService from 'api/CRUD/ProductOrdersService';
+import ProductsService from 'api/CRUD/ProductsService';
 import ProductTranslationsService from 'api/CRUD/ProductTranslationsService';
 import ReviewsService from 'api/CRUD/ReviewsService';
-import UsersService from 'api/CRUD/UsersService';
+import ShippingAddressesService from 'api/CRUD/ShippingAddressesService';
+import SizeOptionsService from 'api/CRUD/SizeOptionsService';
+import UserOrderHistoryService from 'api/CRUD/UserOrderHistoryService';
 import UserProfilesService from 'api/CRUD/UserProfilesService';
 import UserRolesService from 'api/CRUD/UserRolesService';
+import UsersService from 'api/CRUD/UsersService';
 
 export const getTableTotalCount = async (currentTable) => {
 
@@ -49,9 +72,6 @@ export const getTableTotalCount = async (currentTable) => {
       break;
     case 'CustomizableProducts':
       response = await CustomizableProductsService.GetCustomizableProductsCount();
-      break;
-    case 'CustomizationOrders':
-      response = await CustomizationOrdersService.GetCustomizationOrdersCount();
       break;
     case 'FabricTypes':
       response = await FabricTypesService.GetFabricTypesCount();
