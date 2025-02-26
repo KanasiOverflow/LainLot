8694b64b8e4d660bd7cbdc62fa76889994a08e36
Kanasi <kanstantsin.asinouski@gmail.com>
2025-02-26 15:24:55 +0100

    Change folders location

--- Frontend/atelier/src/components/UI/footer/Footer.jsx
+++ Frontend/atelier/src/components/UI/footer/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import Copyright from '../copyright/Copyright';
-import Footbar from '../footbar/Footbar';
+import Copyright from './copyright/Copyright';
+import Footbar from './footbar/Footbar';
 
 export default function Footer() {
     return (
--- Frontend/atelier/src/components/UI/header/Header.jsx
+++ Frontend/atelier/src/components/UI/header/Header.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
-import Sidebar from '../sidebar/Sidebar';
-import Navbar from '../navbar/Navbar';
+import Sidebar from './sidebar/Sidebar';
+import Navbar from './navbar/Navbar';
 import mcss from './Header.module.css';
 
 export default function Header() {
     return (
-        <div className={mcss.headerSection}>
-            <div className={"container"}>
-                <div className={mcss.containtMain}>
-                    <Sidebar />
-                    <Navbar />
+        <div className={"banner_bg_main"}>
+            <div className={mcss.headerSection}>
+                <div className={"container"}>
+                    <div className={mcss.containtMain}>
+                        <Sidebar />
+                        <Navbar />
+                    </div>
                 </div>
             </div>
         </div>
