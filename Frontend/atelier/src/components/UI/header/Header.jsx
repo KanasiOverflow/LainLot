75b8fd2a497beb4441d280ae35ddeaebb4c26175
Kanasi <kanstantsin.asinouski@gmail.com>
2025-02-26 17:07:50 +0100

    Add menu

--- Frontend/atelier/src/components/UI/header/Header.jsx
+++ Frontend/atelier/src/components/UI/header/Header.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Sidebar from './sidebar/Sidebar';
 import Navbar from './navbar/Navbar';
+import Menu from './menu/Menu';
 import mcss from './Header.module.css';
 
 export default function Header() {
     return (
         <div className={"banner_bg_main"}>
+            <Menu />
             <div className={mcss.headerSection}>
                 <div className={"container"}>
                     <div className={mcss.containtMain}>
