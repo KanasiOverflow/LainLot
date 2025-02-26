d2cbaf9b9224f05332995218e838422f3b098bb9
Kanasi <kanstantsin.asinouski@gmail.com>
2025-02-26 17:13:38 +0100

    Add logo section

--- Frontend/atelier/src/components/UI/header/Header.jsx
+++ Frontend/atelier/src/components/UI/header/Header.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Sidebar from './sidebar/Sidebar';
 import Navbar from './navbar/Navbar';
 import Menu from './menu/Menu';
+import Logo from './logo/Logo';
 import mcss from './Header.module.css';
 
 export default function Header() {
     return (
         <div className={"banner_bg_main"}>
             <Menu />
+            <Logo />
             <div className={mcss.headerSection}>
                 <div className={"container"}>
                     <div className={mcss.containtMain}>
