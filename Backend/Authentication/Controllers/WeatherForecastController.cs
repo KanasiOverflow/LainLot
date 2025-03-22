ba33da06463b83b39da8ad98cf25b8abd97f51f0
Kanasi <kanstantsin.asinouski@gmail.com>
2025-03-22 16:23:14 +0100

    Fix swagger auth

--- Backend/Authentication/Controllers/WeatherForecastController.cs
+++ Backend/Authentication/Controllers/WeatherForecastController.cs
@@ -3,31 +3,36 @@ using Microsoft.AspNetCore.Mvc;
 namespace Authentication.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    [Route("api/auth/[controller]")]
+    public class WeatherForecastAuthController(ILogger<WeatherForecastAuthController> logger) : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
+        private static readonly string[] Summaries =
+    [
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    ];
 
-        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly ILogger<WeatherForecastAuthController> _logger = logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        [HttpGet]
+        public IEnumerable<WeatherForecastAuth> Get()
         {
-            _logger = logger;
-        }
-
-        [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
+            return [.. Enumerable.Range(1, 5).Select(index => new WeatherForecastAuth
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+            Date = DateTime.Now.AddDays(index),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })];
         }
     }
+}
+
+public class WeatherForecastAuth
+{
+    public DateTime Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
 }
