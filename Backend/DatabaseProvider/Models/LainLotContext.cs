154c9c1f410bf60a44d4a6531d28fe84d393fe47
Kanasi <kanstantsin.asinouski@gmail.com>
2025-03-30 20:16:46 +0200

    ConfirmationTokenExpires add time zone to the field

--- Backend/DatabaseProvider/Models/LainLotContext.cs
+++ Backend/DatabaseProvider/Models/LainLotContext.cs
@@ -701,7 +701,7 @@ public partial class LainLotContext : DbContext
             entity.HasIndex(e => e.Login, "Users_Login_key").IsUnique();
 
             entity.Property(e => e.ConfirmationTokenExpires)
-                .HasColumnType("timestamp without time zone");
+                .HasColumnType("timestamp with time zone");
             entity.Property(e => e.CreatedAt)
                 .HasDefaultValueSql("CURRENT_TIMESTAMP")
                 .HasColumnType("timestamp without time zone");
