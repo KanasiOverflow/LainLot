ba6235fc3220d5623b4ecc78cda44d40ecdd2dec
Kanasi <kanstantsin.asinouski@gmail.com>
2025-04-16 16:48:54 +0200

    Import base glb files ans styles

--- Frontend/atelier/src/components/ModelViewer/ModelViewer.jsx
+++ Frontend/atelier/src/components/ModelViewer/ModelViewer.jsx
@@ -1,6 +1,7 @@
-import { Canvas } from '@react-three/fiber'
-import { OrbitControls, useGLTF } from '@react-three/drei'
-import { useEffect } from 'react'
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, useGLTF } from '@react-three/drei';
+import { useEffect } from 'react';
+import mcss from './ModelViewer.module.css';
 
 const Model = ({ settings, url }) => {
   const { scene } = useGLTF(url)
@@ -10,6 +11,7 @@ const Model = ({ settings, url }) => {
       const mesh = scene.getObjectByName(name)
       if (mesh && mesh.material) mesh.material.color.set(color)
     })
+    // eslint-disable-next-line
   }, [settings])
 
   return <primitive object={scene} />
@@ -27,11 +29,13 @@ export default function ModelViewer() {
   }
 
   return (
-    <Canvas camera={{ position: [0, 1.5, 3] }}>
-      <ambientLight intensity={0.5} />
-      <directionalLight position={[2, 2, 2]} />
-      <Model settings={settings} url={url} />
-      <OrbitControls />
-    </Canvas>
+    <div className={mcss.viewerWrapper}>
+      <Canvas className={mcss.canvasContainer} camera={{ position: [0, 1.5, 3] }}>
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[2, 2, 2]} />
+        <Model settings={settings} url={url} />
+        <OrbitControls />
+      </Canvas>
+    </div>
   )
 }
