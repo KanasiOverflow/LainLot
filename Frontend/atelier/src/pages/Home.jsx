ebab087f933d348de2700fb04ca486963e5a74be
Kanasi <kanstantsin.asinouski@gmail.com>
2025-05-14 08:50:45 +0200

    Change project structure

--- Frontend/atelier/src/pages/Home.jsx
+++ Frontend/atelier/src/pages/Home.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import ModelViewer from '../components/ModelViewer/ModelViewer.jsx';
 
 export default function About() {
   return (
     <h1>
       Home page
-      <ModelViewer />
+
     </h1>
   );
 }
