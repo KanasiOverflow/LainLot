0fe2ce2bc04836a42518d8f3599e1601a49fa0ec
Kanasi <kanstantsin.asinouski@gmail.com>
2025-09-17 15:38:12 +0200

    Upgrade all nuget packages. Fix issues after that

--- Backend/RestAPI/Controllers/DatabaseController.cs
+++ Backend/RestAPI/Controllers/DatabaseController.cs
@@ -65,7 +65,7 @@ namespace RestAPI.Controllers
         /// CTRL + M + O - collapse all
         /// </summary>
 
-        private readonly Mapper _mapper = MapperConfig.InitializeAutomapper();
+        private readonly IMapper _mapper = MapperConfig.InitializeAutomapper();
         #region repos init
         private readonly ILogger<DatabaseController> _logger = logger;
         private readonly IRepository<DB.About> _aboutRepository = aboutRepository;
